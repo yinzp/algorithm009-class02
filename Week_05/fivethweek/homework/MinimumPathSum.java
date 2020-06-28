76641d44a2709da30ca3c40992246a9b993a3263
yinzp <yinzp18@163.com>
2020-06-28 23:20:44 +0800

    提交第六周作业

---
+++ Week_05/fivethweek/homework/MinimumPathSum.java
@@ -0,0 +1,43 @@
+package com.algorithm.fivethweek.homework;
+
+public class MinimumPathSum {
+
+    public static int[][] grid = {
+            {1, 3, 1},
+            {1, 5, 1},
+            {4, 2, 1}
+    };
+
+    public static void main(String[] args) {
+        //minPathSum(grid);
+        minPathSum1(grid);
+    }
+
+    public static int minPathSum(int[][] grid) {
+        return calculate(grid, 0, 0);
+    }
+
+    public static  int calculate(int[][] grid, int i, int j) {
+        if (i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
+        if (i == grid.length - 1 && j == grid[0].length - 1) return grid[i][j];
+        return grid[i][j] + Math.min(calculate(grid, i + 1, j), calculate(grid, i, j + 1));
+    }
+
+    public static int minPathSum1(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
+                if(i == grid.length - 1 && j != grid[0].length - 1)
+                    dp[i][j] = grid[i][j] +  dp[i][j + 1];
+                else if(j == grid[0].length - 1 && i != grid.length - 1)
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                else if(j != grid[0].length - 1 && i != grid.length - 1)
+                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+                else
+                    dp[i][j] = grid[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+
+}

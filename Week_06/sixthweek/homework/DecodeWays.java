fa78c576271db5515d9ac437a025f096475396d8
yinzp <yinzp18@163.com>
2020-06-28 23:24:20 +0800

    提交第六周作业

---
+++ Week_06/sixthweek/homework/DecodeWays.java
@@ -0,0 +1,29 @@
+package com.algorithm.sixthweek.homework;
+
+public class DecodeWays {
+
+    private static String mString = "12";
+
+    public static void main(String[] args) {
+        numDecodings(mString);
+    }
+
+    public static int numDecodings(String s) {
+        char[] nums = s.toCharArray();
+        int len = nums.length;
+        int[] dp = new int[len+1];  // dp[i] 表示从第i+1个数到第n个数的所有方案数
+        dp[len] = 1;
+        // 从右往左
+        for(int i = len-1; i >= 0; i--) {
+            // 注意判断0字符
+            if (nums[i] == '0') continue;   // 当开始位为0字符时不满足任意一个字母的解析，跳过
+            int num = 0;
+            for (int j = i; j < len && j-i<2; j++) {
+                num = num*10 + (nums[j]-'0');
+                // 对子状态dp[j+1]为0开头的也可进行添加，因为没有赋值为dp[j+1]为0
+                if (num <= 26) dp[i] += dp[j+1];
+            }
+        }
+        return dp[0];
+    }
+}
---
+++ Week_06/sixthweek/homework/MaximalSquare.java
@@ -0,0 +1,35 @@
+package com.algorithm.sixthweek.homework;
+
+public class MaximalSquare {
+
+    public static char[][] grid = {
+            {1, 0, 1, 0, 0},
+            {1, 0, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 1, 0}
+    };
+
+    public static void main(String[] args) {
+        maximalSquare(grid);
+    }
+    public static int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
+
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int maxSide = 0;
+
+        // 相当于已经预处理新增第一行、第一列均为0
+        int[][] dp = new int[height + 1][width + 1];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (matrix[row][col] == '1') {
+                    dp[row + 1][col + 1] = Math.min(Math.min(dp[row + 1][col], dp[row][col + 1]), dp[row][col]) + 1;
+                    maxSide = Math.max(maxSide, dp[row + 1][col + 1]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
